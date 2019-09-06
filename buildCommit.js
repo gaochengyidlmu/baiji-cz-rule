4e2c8d580ba97493c94ddeed5fbfd7e6383914c4
高承毅 <gaochengyidlmu@163.com>
2019-09-06 13:18:08 +0800

    【更新】  模块: commit  原因: commit 成为一行

--- buildCommit.js
+++ buildCommit.js
@@ -16,9 +16,9 @@ module.exports = function buildCommit(answers, config) {
   };
 
   function addScope(scope) {
-    if (!scope) return '\n'; //it could be type === WIP. So there is no scope
+    if (!scope) return '  '; //it could be type === WIP. So there is no scope
 
-    return ': ' + scope.trim() + '\n';
+    return ': ' + scope.trim() + '  ';
   }
 
   function addSubject(subject) {
@@ -42,7 +42,7 @@ module.exports = function buildCommit(answers, config) {
   var head = (answers.type + addScope(answers.scope) + addSubject(answers.subject)).slice(0, maxLineWidth);
   // Wrap these lines at 100 characters
   var body = wrap(answers.body, wrapOptions) || '';
-  body = body.split('|').join('\n');
+  body = body.split('|').join('  ');
 
   var breaking = wrap(answers.breaking, wrapOptions);
   var footer = wrap(answers.footer, wrapOptions);
@@ -50,15 +50,15 @@ module.exports = function buildCommit(answers, config) {
   var result = head;
   if (body) {
     var bodyPrefix = (entry[answers.type].body && entry[answers.type].body.prefix) || '';
-    result += '\n' + bodyPrefix + body;
+    result += '  ' + bodyPrefix + body;
   }
   if (breaking) {
     var breakingPrefix = entry && entry[answers.type].breaking && entry[answers.type].breaking.prefix ? entry[answers.type].breaking.prefix : 'BREAKING CHANGE:';
-    result += '\n' + breakingPrefix + '\n' + breaking;
+    result += '  ' + breakingPrefix + '  ' + breaking;
   }
   if (footer) {
     var footerPrefix = entry && entry[answers.type].footer.prefix ? entry[answers.type].footer.prefix : 'ISSUES CLOSED:';
-    result += '\n' + footerPrefix + footer;
+    result += '  ' + footerPrefix + footer;
   }
   return escapeSpecialChars(result);
 };
